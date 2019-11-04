b5c1722d36611dfbfa481e1d1596b997ff77ad63
Utilisateur <Utilisateur@10.200.28.170>
2019-11-04 18:35:13 -0500

    Commit

---
+++ tp2/TP2Pourvoirie/Test/implement/lodgeMock/LodgeMock.java
@@ -0,0 +1,14 @@
+package implement.lodgeMock;
+
+import implementation.lodgeOptions.FourPeopleLodge;
+
+public class LodgeMock extends FourPeopleLodge{
+
+	public final static int NB_PEOPLE = 0;
+	public final static int NB_DAYS = 0;
+	
+	public LodgeMock(int numberOfNight, int numberOfPeople) {
+		super(numberOfNight, numberOfPeople);
+	}
+
+}
